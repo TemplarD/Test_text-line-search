0b053978bd7fdc1c8f0fb2d0d7d20ad3e67fe47b
TemplarD <kirov_daniel@mail.ru>
2024-02-11 20:53:42 +0300

    Start Commit

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls;
 
 type
-  TForm1 = class(TForm)
+  TMyForm = class(TForm)
+    TextEditor: TMemo;
+    PrintString: TEdit;
+    SelectionNumberString: TComboBox;
+    StartSelection: TButton;
+    SumB: TButton;
+    procedure StartSelectionClick(Sender: TObject);
+    procedure SumBClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +22,51 @@ type
   end;
 
 var
-  Form1: TForm1;
+  MyForm: TMyForm;
 
 implementation
 
 {$R *.dfm}
+
+procedure TMyForm.StartSelectionClick(Sender: TObject);
+var 
+  SelectionIndex: Integer; //хранение инедекса SelectionNumberString выбранного номера строки
+  begin
+    try
+      begin  
+      selectionIndex := SelectionNumberString.ItemIndex;
+        if (selectionIndex <> -1) and (SelectionNumberString.Text <> '') then
+          begin
+          PrintString.Text := TextEditor.Lines[selectionIndex];
+          end
+        else
+        ShowMessage('Параметр не выбран');
+      end;
+    except
+    ShowMessage('Произошла не предвиденная ошибка, при выборе и выводе строки!');
+    end;
+  end;
+
+procedure TMyForm.SumBClick(Sender: TObject);
+var
+  Count: Integer;  // хранение числа вхождений в текст компонента TextEditor искомых символов.
+  i: Integer;
+  TextToSearch: string; // хранение текста приведенного к нижнему регистру, для избежания дублирования логики при поиске символов
+  begin
+    try
+      begin
+      TextToSearch := AnsiLowerCase(TextEditor.Text);
+      Count := 0;
+        for i := 1 to Length(TextToSearch) do
+          begin
+            if TextToSearch[i] = 'б' then
+            inc(Count);
+          end;
+      PrintString.Text := IntToStr(Count);
+      end;
+    Except
+    ShowMessage('Произошла не предвиденная ошибка, при подсчете символов б!');
+    end;
+  end;
 
 end.
